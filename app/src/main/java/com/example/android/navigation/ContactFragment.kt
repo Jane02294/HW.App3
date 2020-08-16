746f0d3f2276e6410269f24a8bca1126f95f4407
Jane02294 <46182833+Jane02294@users.noreply.github.com>
2020-08-16 22:06:33 +0700

    add menu contact

--- app/src/main/java/com/example/android/navigation/ContactFragment.kt
+++ app/src/main/java/com/example/android/navigation/ContactFragment.kt
@@ -10,6 +10,6 @@ class ContactFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_contact, container, false)
     }
 }
